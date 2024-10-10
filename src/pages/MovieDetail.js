4c4e853275388ff38909d287b1a3678995a687e6
Your Mughees <mugheesbutt12@gmail.com>
2024-10-10 04:10:43 +0100

    Updated useEffect Dependency

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -16,7 +16,7 @@ export const MovieDetail = () => {
       console.log(json);
     }
     fetchMovie();
-  }, [])
+  }, [params.id])
   return (
     <main>
       <section className="flex justify-around flex-wrap py-5">
